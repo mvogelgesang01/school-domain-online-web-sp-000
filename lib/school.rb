12ddcb23dbd6e9c6bbc68860c542a2f12cad7714
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-04-30 22:22:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,4 +3,23 @@ class School
     @name = name
     @roster = {}
   end
-    
+
+    def roster
+      @roster
+    end 
+
+    def add_student(name, grade)
+      @roster[grade] ||= []
+      @roster[grade] << name
+    end
+
+    def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
